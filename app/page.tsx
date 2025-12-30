567748dcb6b2df8a6467af79450e72afaf40569f
Sanjoy <guinsanjoy34@gmail.com>
2025-12-30 11:37:06 +0530

    feat : diff fork text added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       i am main branch edit
       <h1> test branch edit , hello everyone</h1>
+      <p>commit from different fork</p>
     </div>
   );
 }
